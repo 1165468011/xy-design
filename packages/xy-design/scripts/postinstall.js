2a7d25c2618487791e1025eca6a04b28e8373bd3
霍洋 <1165468011@qq.com>
2022-11-14 18:30:39 +0800

    chore: 新增类型检查

--- packages/xy-design/scripts/postinstall.js
+++ packages/xy-design/scripts/postinstall.js
@@ -2,14 +2,12 @@ const { switchVersion, loadModule } = require("./utils");
 const Vue = loadModule("vue");
 if (!Vue || typeof Vue.version !== "string") {
   console.warn(
-    '[vue3-sketch-ruler] Vue is not found. Please run "npm install vue" to install.'
+    '[xy-design] Vue is not found. Please run "npm install vue" to install.'
   );
 } else if (Vue.version.startsWith("2.")) {
   switchVersion(2);
 } else if (Vue.version.startsWith("3.")) {
   switchVersion(3);
 } else {
-  console.warn(
-    `[vue3-sketch-ruler] Vue version v${Vue.version} is not suppported.`
-  );
+  console.warn(`[xy-design] Vue version v${Vue.version} is not suppported.`);
 }
